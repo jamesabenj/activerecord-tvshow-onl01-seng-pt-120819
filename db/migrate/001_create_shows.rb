153745fc92576c54a70406c4498cf4c04be4ec43
James Benjamin <james.abenj@gmail.com>
2020-03-02 20:19:36 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,8 +1,11 @@
-def change
-  create_table :shows do |t|
-    t.string :name
-    t.string :network
-    t.string :day
-    t.integer :rating
-  end
+class CreateShows < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+
 end
