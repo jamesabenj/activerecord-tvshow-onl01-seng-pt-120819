ca83b04495bb0730a83798223abee5e01d0e61d7
James Benjamin <james.abenj@gmail.com>
2020-03-02 21:08:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,11 @@
 class Show < ActiveRecord::Base
 
-  def self.highest_rating
+  def Show::highest_rating
     Show.maximum(:rating)
   end
 
-  def self.most_popular_show
-    Show.where("rating = ?", Show.maximum(:rating))
+  def Show::most_popular_show
+    Show.where("rating = ?", highest_rating)
   end
 
   def lowest_rating
